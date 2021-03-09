c0a6151f8a2fb1f0ba893a4aec929e014839b0f3
Lynne16 <76992610+Lynne16@users.noreply.github.com>
2021-03-09 14:32:37 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAc3F-rtA0Oo0TB5lEUuG-bdkrFgGtV8aM",
+    authDomain: "kwitter-project-36d3e.firebaseapp.com",
+    databaseURL: "https://kwitter-project-36d3e-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-36d3e",
+    storageBucket: "kwitter-project-36d3e.appspot.com",
+    messagingSenderId: "438296149964",
+    appId: "1:438296149964:web:2911153471c8250572e769"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+// FIREBASE LINKS
+function logOut(){
+    window.location="index.html";
+}
